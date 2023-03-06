79dc3c06ba2dcf5dfc2124296d48026bbff0fbd5
latted23 <latted23@gmail.com>
2023-03-06 09:17:21 +0900

    upload

--- flask_upload.py
+++ flask_upload.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
+import socket
 import os
+
+
 app = Flask(__name__)
 #app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 #파일 업로드 용량 제한 단위:바이트
 
@@ -33,4 +36,4 @@ def upload_file():
 
 #서버 실행
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5500, debug = True)
+	app.run(host='0.0.0.0', port="5500", debug = True)
