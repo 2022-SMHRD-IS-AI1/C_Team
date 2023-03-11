7e2af7a82267778bbd8576895716bba6edc27335
이창현 <120704794+rchea0123@users.noreply.github.com>
2023-03-11 11:10:18 +0900

    keybert 수정

--- file_list.py
+++ file_list.py
@@ -4,7 +4,7 @@ from kiwipiepy import Kiwi
 import numpy as np
 from keybert import KeyBERT
 
-file_list = os.listdir('./C_team/new_folder')
+file_list = os.listdir('./new_folder')
 # print(file_list)
 kiwi = Kiwi(load_default_dict=True)
 #kiwi.load_user_dictionary('user_dictionary.txt')
@@ -16,8 +16,8 @@ kiwi.add_user_word("AR", "NNG")
 kw_model = KeyBERT(model='paraphrase-distilroberta-base-v1')
 
 
-for i in range(100):
-    text = docx2txt.process('./C_team/new_folder/'+file_list[i])
+for i in range(10):
+    text = docx2txt.process('./new_folder/'+file_list[i])
 
     # print(text)
     # 용언품사 = ['VV', 'VA']
@@ -31,12 +31,12 @@ for i in range(100):
 
     # KeyBert 활용하여 keyword추출
     #stop_words=None 불용어 제거 하는 기준 None ex) stop_words=['3d']
-    keywords = kw_model.extract_keywords(cont, keyphrase_ngram_range=(1, 1), stop_words=None, top_n=10)
+    keywords = kw_model.extract_keywords(cont, keyphrase_ngram_range=(1, 2), stop_words=None, top_n=10)
     #np_keywords = np.array(keywords)
     # print(keywords[0][0])
     #print(np_keywords[0])
 
-    topic_list.append(keywords[0])
+    topic_list.append(keywords[0][0])
 
 # print(file_list[:10])
 print(topic_list)
@@ -50,7 +50,7 @@ print(topic_list)
 # from keybert import KeyBERT
 # from multiprocessing import Pool
 
-# file_list = os.listdir('./new_folder')[:200]
+# file_list = os.listdir('./new_folder')[:100]
 # kiwi = Kiwi(load_default_dict=True)
 # kiwi.add_user_word("3D", "NNG")
 # kiwi.prepare()
@@ -67,3 +67,4 @@ print(topic_list)
 
 # print(file_list)
 # print(topic_list)
+
