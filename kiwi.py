aaa561efb41ed6f972b28316aaf90bfac31038b4
이창현 <120704794+rchea0123@users.noreply.github.com>
2023-03-15 10:39:00 +0900

    kiwi 파일 수정

--- kiwi.py
+++ kiwi.py
@@ -1,52 +1,49 @@
-# from kiwipiepy import Kiwi
-# kiwi = Kiwi()
-# #kiwi.load_user_dictionary('user_dictionary.txt')
-# kiwi.prepare()
-
-# result = kiwi.tokenize('테스트입니다.')
-# for token in result:
-#     print(f"{token.form}\t{token.tag}")
-
-# import pandas as pd
-# #df = pd.read_excel('rawdata.xlsx')
-# df = pd.read_csv('./data/ratings_test.csv')
-# morph_analysis = lambda x: kiwi.tokenize(x) if type(x) is str else None
-# df['형태소분석결과'] = df['document'].apply(morph_analysis)
-
-# print(df['형태소분석결과'].head())
-
-
-# from collections import Counter
-# '''주요 품사, 용언 품사 정의'''
-# 주요품사 = ['NNG', 'NNP', 'VV', 'VA', 'XR', 'SL']
-# 용언품사 = ['VV', 'VA']
-# '''Counter를 활용해 가장 많이 나온 n개의 품사 결과를 돌려주는 pos_count() 함수'''
-# def pos_count(df, col, output_filename, n=100):
-#     카운터 = Counter()
-    
-#     for index, row in df.iterrows(): 
-#         if row[col]:
-#             필터링결과 = [(token.form, token.tag) for token in row[col] if token.tag in 주요품사]
-#             카운터.update(필터링결과)
-            
-#     with open(output_filename, "w", encoding='utf-8-sig') as output_file:
-#         print("형태소,품사,개수", file=output_file)
-#         for (형태소, 품사), 개수 in 카운터.most_common(n):
-#             if 품사 in 용언품사:
-#                 형태소 += "다"
-#             print(f"{형태소},{품사},{개수}", file=output_file)
-
-# pos_count(df, '형태소분석결과', './data/주요어휘빈도.csv')
-
 import os
 import docx2txt
-from keybert import KeyBERT
+from kiwipiepy import Kiwi
+# from kiwipiepy.utils import Stopwords
 
+kiwi = Kiwi(load_default_dict=True)
+kiwi.load_user_dictionary('userDict.txt')
+# stopwords = ['개발', '연구', '향상', '전략', '과정', '문제', '정의', '배포', '지원', '결과',
+#                '솔루션', '테스트', '시장', '진출', '높이기', '반영', '감시', '목적', '대상', '기능', '기술', '단계',
+#                  '방법', '활용', '환경', '사용', '라이브러리', '사용자', '이해', '적용', '성과', '개선', '개월', '계획',
+#                    '고려', '가치', '수립', '목표', '포함', '결정', '참가자', '여부', '내용', '시간', '배경', '제공',
+#                      '이용', '준지', '진행', '도모', '발전', '상품', '정보', '기반', '사업', '처리', '입니다', '합니다',
+#                        '같습니다', '것입니다', '있습니다', '돕는다', '됩니다', '시킵니다', '다음과', '다음은', '이상', '본', '모든', '빠른',
+#                         '빠르고', '정확한', '위함', '은', '는', '이', '가', '을', '를', '하여', '하기', '및', '에서', '과', '와', '의',
+#                          '위한', '으로', '이고', '시키기', '시키고', '것', '위해', '있는', '등', '통해', '통하여', '통해서',
+#                           '하는', '한', '있도록', '더', '에서', '보다', '이를', '같은', '다른', '느끼는', '할 수', '쉽게',
+#                            '원하는', '새로운', '하고', '시키는', '각각의', '하며', '또한', '거쳐', '등의', '된', '하거나', '가장',
+#                             '최적의', '기반으로', '테스트', '다양한', '이런', '이러한', '에서', '나는', '해서', '따라', '따라서', '에',
+#                              '하게', '보다', '이용한', '만들고', '사용한', '활용한', '1.', '2.', '3.', '4.', '5.', '6.',
+#                               '7.', '8.', '9.', '(1)', '(2)', '(3)', '(4)', '(5)', '(6)', ' (7)', '(8)', '(9)', '주요',
+#                                '중요한', '매우', '것이', '있고', '하도록', '이루기', '로', '여러', '되며', '-', '.', ',',
+#                                 '따른', '두고', '담고', '있도록', '모으고', '모아', '담아', '두고', '의도한', '해당', '형식', '경우',
+#                                  '추가']
+# stopwords = Stopwords(filename='stopwords.txt')
+# for i in stop_words:
+#     stopwords.add(i)
+# stopwords.save('stopwords.txt')
+kiwi.prepare()
 
 file_list = os.listdir('./new_folder')
-text = docx2txt.process('./new_folder/'+file_list[0])
 
-kw_model = KeyBERT()
-keywords = kw_model.extract_keywords(text, keyphrase_ngram_range=(3, 3), stop_words=None,
-                                     use_maxsum=True, nr_candidates=20, top_n=5)
-print(keywords)
+def tokenize(file_name):
+    doc = ''
+    # token = []
+    result = ''
+
+    #형용사 = ('VA')
+    #용언품사 = ('VV', 'VA')
+    명사 = ('NN')
+
+    doc = docx2txt.process('./new_folder/'+file_name)
+    kiwi_tokenize = kiwi.tokenize(doc)
+
+    for i in kiwi_tokenize:
+        if i[1].startswith(명사):
+            # token.append(i[0])
+            result += i[0] + ' '
+  
+    return result
