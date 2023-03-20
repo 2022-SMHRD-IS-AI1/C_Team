6fc8ace264b2f29c65feb6a944c08f060d68b538
이창현 <120704794+rchea0123@users.noreply.github.com>
2023-03-20 14:34:04 +0900

    테스트

--- lda_model2.py
+++ lda_model2.py
@@ -53,10 +53,10 @@ async def classification(user_id, file_path):
     lda_model = LdaModel(corpus=corpus,
                         id2word=dictionary,
                         random_state=1,
-                        num_topics=10, # 주제 개수 설정
+                        num_topics=3, # 주제 개수 설정
                         passes=20, # 알고리즘 반복 횟수 설정
                         alpha='auto',
-                        eta="auto",
+                        eta='auto',
                         iterations=400,
                         eval_every=None)
 
